c3585c0f6db343f4ae2959e68355ef532c8ed523
jreleaserbot <jreleaser@kordamp.org>
2023-11-12 14:51:59 +0000

    spring-cli-standalone v0.7.4

--- Formula/spring-cli.rb
+++ Formula/spring-cli.rb
@@ -1,27 +1,28 @@
-# Generated with JReleaser 1.9.0-SNAPSHOT at 2023-11-01T09:27:34.658370262Z
+# Generated with JReleaser 1.10.0-SNAPSHOT at 2023-11-12T14:51:23.915246609Z
 class SpringCli < Formula
   desc "Spring Cli"
   homepage "https://github.com/spring-projects-experimental/spring-cli"
-  version "0.7.3"
+  version "0.7.4"
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/spring-projects-experimental/spring-cli/releases/download/v0.7.3/spring-cli-standalone-0.7.3-linux.x86_64.zip"
-    sha256 "8b4a5e0f2dd198a5422893537cd4b0b83ea73f6203b741212f43d89b8900f4ed"
+    url "https://github.com/spring-projects-experimental/spring-cli/releases/download/v0.7.4/spring-cli-standalone-0.7.4-linux.x86_64.zip"
+    sha256 "e780c20f0cb2c5cf5bf29e5288e272d270002da7a30e18e2bcb45223b4914ef5"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/spring-projects-experimental/spring-cli/releases/download/v0.7.3/spring-cli-standalone-0.7.3-osx.aarch64.zip"
-    sha256 "f1a4a00454fbde5607a985cee48c5ce54035cd59dc2317aa26964f983766612c"
+    url "https://github.com/spring-projects-experimental/spring-cli/releases/download/v0.7.4/spring-cli-standalone-0.7.4-osx.aarch64.zip"
+    sha256 "2644aed5b8d2a3688694c22a6d749ab77ba9251cc35907cc27a2a258ceb2b6ec"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/spring-projects-experimental/spring-cli/releases/download/v0.7.3/spring-cli-standalone-0.7.3-osx.x86_64.zip"
-    sha256 "a74124dd2161b5104e30674454176a122d202183000aaf72ab727cfd6f647cdf"
+    url "https://github.com/spring-projects-experimental/spring-cli/releases/download/v0.7.4/spring-cli-standalone-0.7.4-osx.x86_64.zip"
+    sha256 "626fc1d74e1b3c23df90e88facc3223c2a14e8db7e91781a22746b8c99760bce"
   end
 
 
   def install
     libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/bin/spring" => "spring"
+    bash_completion.install Dir["#{libexec}/completion/bash/spring"]
   end
 
   def post_install
@@ -37,6 +38,6 @@ class SpringCli < Formula
 
   test do
     output = shell_output("#{bin}/spring --version")
-    assert_match "0.7.3", output
+    assert_match "0.7.4", output
   end
 end
